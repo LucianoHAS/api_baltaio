61f0ecf688bba9599f90d36e5e4e8717720f9806
LucianoHAS <luciano.has@gmail.com>
2018-12-30 23:08:57 -0200

    Rotas de CRUD

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: 'Node Store API',
@@ -12,6 +15,31 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
+
 
 module.exports = app;

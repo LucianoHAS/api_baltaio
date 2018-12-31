7e242ac57016b585abf7aaaeef2d189ddc94040f
LucianoHAS <luciano.has@gmail.com>
2018-12-31 00:24:10 -0200

    Rotas em arquivo separado

--- src/app.js
+++ src/app.js
@@ -1,45 +1,22 @@
 'use strict'
 
 const express = require('express');
-
 const app = express();
-const router = express.Router();
 
+
+// Carregamento das rotas
+const indexRoutes = require('./routes/index');
+const productsRoutes = require('./routes/products');
+
+
+// Configuração
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'Node Store API',
-        version: '0.0.1'
-    });
-});
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+// Rotas
+app.use('/', indexRoutes);
+app.use('/products', productsRoutes);
 
 
 module.exports = app;
